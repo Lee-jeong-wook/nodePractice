74d7014cb40447e52658d3e2c77f0c330bdaf982
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-08-19 22:48:55 +0900

    간단한 로그인 구현

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const PORT = 3000;
@@ -8,9 +9,11 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', "./src/views");
 app.set('view engine', 'ejs');
-
 // 미들웨어
 app.use(express.static(`${__dirname}/src/public`))
+app.use(bodyParser.json());
+//인코딩 위함
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/', home);
 
